94d4e00a3fe49224f0e9ff3b8d87b29d45869493
PyAde <108620649+PyAde@users.noreply.github.com>
2022-10-11 17:17:59 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors : {
+        magenta: '#d97706',
+        abu: '#64748b',
+        dark : '#111827',
+        yello : '#ea580c',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
